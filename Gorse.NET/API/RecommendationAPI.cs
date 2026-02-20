8e8ee96ea7dc4d08018c14432e96b6a575256557
Ibrahim Habibeh <Ibrahimhabibeh@gmail.com>
2026-02-20 13:20:51 -0800

    remove nullability for parameter n

--- Gorse.NET/API/RecommendationAPI.cs
+++ Gorse.NET/API/RecommendationAPI.cs
@@ -5,12 +5,12 @@ namespace Gorse.NET;
 
 public partial class Gorse
 {
-    public string[]? GetRecommend(string userId, int? n = 10)
+    public string[]? GetRecommend(string userId, int n = 10)
     {
         return _client.Request<string[], Object>(Method.Get, $"api/recommend/{userId}?n={n}", null);
     }
 
-    public Task<string[]?> GetRecommendAsync(string userId, int? n = 10)
+    public Task<string[]?> GetRecommendAsync(string userId, int n = 10)
     {
         return _client.RequestAsync<string[], Object>(Method.Get, $"api/recommend/{userId}?n={n}", null);
     }
@@ -25,7 +25,7 @@ public partial class Gorse
         return _client.RequestAsync<List<UserScore>, object>(Method.Get, $"api/user/{userId}/neighbors?n={n}&offset={offset}", null)!;
     }
 
-    public Task<List<UserScore>> GetRecommendLatestAsync(int? n = 10)
+    public Task<List<UserScore>> GetRecommendLatestAsync(int n = 10)
     {
         return _client.RequestAsync<List<UserScore>, object>(Method.Get, $"api/latest?n={n}", null)!;
     }
